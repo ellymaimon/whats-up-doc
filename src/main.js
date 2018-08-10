80523a0678abba880313cd71a64448c5e575036b
Elly Maimon <eliranmaimon@gmail.com>
2018-08-10 16:05:48 -0700

    Refactor and include cases for condition, name, both, or neither

--- src/main.js
+++ src/main.js
@@ -14,10 +14,29 @@ function isAccepting(element) {
   else return "The doctor is currently not accepting new patients."
 }
 
-$(document).ready(function(){
-  let doctor = new Doctor();  // create instance of Doctor class
-  let conditionsPromise = doctor.getConditions();
+function displayDoctors(doctor) {
+  let firstName = doctor.profile.first_name;
+  let lastName = doctor.profile.last_name;
+  let address = doctor.practices[0].visit_address.street;
+  let address2 = doctor.practices[0].visit_address.street2;
+  let city = doctor.practices[0].visit_address.city;
+  let state = doctor.practices[0].visit_address.state;
+  let zip = doctor.practices[0].visit_address.zip;
+  let phone = doctor.practices[0].phones[0].number;
+  let website = doctor.practices[0].website;
+  let accepts = doctor.practices[0].accepts_new_patients;
 
+  $("#results").append(`<div class='card'><div class='card-body'>
+  <h5 class='card-title doctor-name'>${firstName} ${lastName}</h5>
+  <p class='card-text'><b>Address:</b> ${address} ${isUndefined(address2) ? "" : address2}, ${city}, ${state}, ${zip}</p>
+  <p class='card-text'><b>Phone:</b> ${phone}</p>
+  <p class='card-text'><b>Website:</b> ${isUndefined(website) ? "No website found." : website}</p>
+  <p class='card-text'>${isAccepting(accepts)}</p></div></div>`);
+}
+
+$(document).ready(function(){
+  let doctor = new Doctor();
+  let conditionsPromise = doctor.getConditions();
 
   conditionsPromise.then(function(response) {
     let body = JSON.parse(response);
@@ -30,52 +49,66 @@ $(document).ready(function(){
     $('.showErrors').text(`There was an error processing your request: ${error.message}`);
   });
 
-
-
-
-  $("#doctor-form").submit(function(event){ //submit the form to search
+  $("#doctor-form").submit(function(event){
     event.preventDefault();
     let doctorName = $("#doctor-name").val();
-    
-    // let condition = ;
-
-    let namePromise = doctor.getDoctorByName(doctorName);  // call the instance method
-
-    // let conditionPromise = doctor.getDoctorByCondition(condition);
-
+    let condition =  $("#condition-select").val();
     $("#results").html("<h3>Searching for the right doc...</h3>");
     
-    namePromise.then(function(response) {
-      let body = JSON.parse(response);
+    if(doctorName === "" && condition == "no") {
       $("#results").html("");
-      //Message if no doctors were found
-      if (isUndefined(body.data[0])) {
-        $("#results").append(`<h3>Sorry, no doctors with that name were found in the area.</h3>`);
-      } 
-      //Display doctors in cards
-      else {
-        body.data.forEach(function(doctor){
-          let firstName = doctor.profile.first_name;
-          let lastName = doctor.profile.last_name;
-          let address = doctor.practices[0].visit_address.street;
-          let address2 = doctor.practices[0].visit_address.street2;
-          let city = doctor.practices[0].visit_address.city;
-          let state = doctor.practices[0].visit_address.state;
-          let zip = doctor.practices[0].visit_address.zip;
-          let phone = doctor.practices[0].phones[0].number;
-          let website = doctor.practices[0].website;
-          let accepts = doctor.practices[0].accepts_new_patients;
+      $("#results").append(`<h3>Please input a name or select a condition.</h3>`); 
+    }
+    else if(condition === "no") {
+      let namePromise = doctor.getDoctorByName(doctorName);
+      namePromise.then(function(response) {
+        let body = JSON.parse(response);
+        $("#results").html("");
+        if (isUndefined(body.data[0])) {
+          $("#results").append(`<h3>Sorry, no doctors with that name were found in the area.</h3>`);
+        } 
+        else {
+          body.data.forEach(function(doctor){
+            displayDoctors(doctor);
+          });
+        } 
+      }, function(error) {
+        $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+      });
+    }
+    else if (doctorName === "") {
+      let conditionPromise = doctor.getDoctorByCondition(condition);
 
-          $("#results").append(`<div class='card'><div class='card-body'>
-          <h5 class='card-title doctor-name'>${firstName} ${lastName}</h5>
-          <p class='card-text'><b>Address:</b> ${address} ${isUndefined(address2) ? "" : address2}, ${city}, ${state}, ${zip}</p>
-          <p class='card-text'><b>Phone:</b> ${phone}</p>
-          <p class='card-text'><b>Website:</b> ${isUndefined(website) ? "No website found." : website}</p>
-          <p class='card-text'>${isAccepting(accepts)}</p></div></div>`);
-        });
-      }
-    }, function(error) {
-      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-    });
+      conditionPromise.then(function(response) {
+        let body = JSON.parse(response);
+        $("#results").html("");
+        if (isUndefined(body.data[0])) {
+          $("#results").append(`<h3>Sorry, no doctors that treat that condition were found in the area.</h3>`);
+        } else {
+          body.data.forEach(function(doctor){
+            displayDoctors(doctor);
+          });
+        }
+      }, function(error) {
+        $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+      });
+    }
+    else {
+      let bothPromise = doctor.getDoctor(name, condition);
+      
+      bothPromise.then(function(response) {
+        let body = JSON.parse(response);
+        $("#results").html("");
+        if (isUndefined(body.data[0])) {
+          $("#results").append(`<h3>Sorry, either no doctors with that name or no doctors who treat that condition were found in the area.</h3>`);
+        } else {
+          body.data.forEach(function(doctor){
+            displayDoctors(doctor);
+          });
+        }
+      }, function(error) {
+        $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+      });
+    }
   });
 });
