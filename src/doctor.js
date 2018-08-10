91f4ce690320e644e84d8eba779f1c9703bd2c8b
Elly Maimon <eliranmaimon@gmail.com>
2018-08-10 16:05:18 -0700

    Add functions to get doctor by condition and by both condition and name

--- src/doctor.js
+++ src/doctor.js
@@ -32,12 +32,37 @@ export class Doctor {
       request.send();
     });
   }
-  // getDoctorByCondition(condition) {
 
-  // }
+  getDoctorByCondition(condition) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${condition}&location=or-portland&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
 
-  // getDoctor(name, condition) {
-
-  // }
+  getDoctor(name, condition) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${condition}&location=or-portland&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
 
 }
