1513e46184ef068512f810d1c41d614507a33916
Elly Maimon <eliranmaimon@gmail.com>
2018-08-10 13:37:50 -0700

    Add searching text and handle case for no doctor found

--- src/main.js
+++ src/main.js
@@ -11,19 +11,31 @@ $(document).ready(function(){
     event.preventDefault();
     let doctorName = $("#doctor-name").val();
     let namePromise = doctor.getDoctorByName(doctorName);  // call the instance method
+    $("#results").html("<h3>Searching for the right doc...</h3>");
     
     namePromise.then(function(response) {
       let body = JSON.parse(response);
-      body.data.forEach(function(doctor){
-        console.log(doctor);
-        $("#results").append(`<div class='card'><div class='card-body'>
-        <h5 class='card-title doctor-name'>${doctor.profile.first_name} ${doctor.profile.last_name}</h5>
-        <p class='card-text address'>${doctor.practices[0].visit_address.street}</p>`);
-        // $("#results").append("<p class='card-text phone'></p>");
-        // $("#results").append("<p class='card-text website'></p>");
-        // $("#results").append("<p class='card-text accepting'></p></div></div>"); 
-      });
-      
+      $("#results").html("");
+      console.log(body.data[0]);
+      //Message if no doctors were found
+      if (body.data[0] === undefined) {
+        $("#results").append(`<h3>Sorry, no doctors with that name were found in the area.</h3>`);
+      } 
+      //Display doctors in cards
+      else {
+        body.data.forEach(function(doctor){
+          console.log(doctor);
+          $("#results").append(`<div class='card'><div class='card-body'>
+          <h5 class='card-title doctor-name'>${doctor.profile.first_name} ${doctor.profile.last_name}</h5>
+          <p class='card-text address'>Address: ${doctor.practices[0].visit_address.street} ${doctor.practices[0].visit_address.street2},
+          ${doctor.practices[0].visit_address.city}, ${doctor.practices[0].visit_address.state},
+          ${doctor.practices[0].visit_address.zip}</p>
+          <p class='card-text phone'>Phone: ${doctor.practices[0].phones[0].number}</p>`);
+          // $("#results").append("<p class='card-text phone'></p>");
+          // $("#results").append("<p class='card-text website'></p>");
+          // $("#results").append("<p class='card-text accepting'></p></div></div>"); 
+        });
+      }
       // console.log(body.data[0].practices[0].visit_address.city);
       // console.log(body.data[0].practices[0].visit_address.state);
       // console.log(body.data[0].practices[0].visit_address.street);
